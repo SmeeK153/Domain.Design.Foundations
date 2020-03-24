f6831c92de33968d3e50ce1814806eee0bdce126
Eric Freeman <eric.freeman@ago.org>
2020-03-24 00:23:10 -0700

    Add missing unit tests

--- Tests/EnumerationTests.cs
+++ Tests/EnumerationTests.cs
@@ -1,56 +1,58 @@
 using System;
+using FluentAssertions;
+using Foundations.Core;
 using Xunit;
 
 namespace Tests
 {
     public class EnumerationTests
     {
+        class TestEnumeration : Enumeration
+        {
+            public static TestEnumeration Enumeration1 { get; } = new TestEnumeration(1, "TestEnumeration1");
+            public static TestEnumeration Enumeration2 { get; } = new TestEnumeration(2, "TestEnumeration2");
+            public static TestEnumeration Enumeration3 { get; } = new TestEnumeration(3, "TestEnumeration3");
+            protected TestEnumeration(int id, string name) : base(id, name)
+            {
+            }
+        }
+        
         [Fact]
         public void LookupExistingEnumerationById()
         {
-
+            Enumeration.FromId<TestEnumeration>(1).Should().Be(TestEnumeration.Enumeration1);
         }
 
         [Fact]
         public void LookupNonexistentEnumerationByIdFails()
         {
-
+            Action lookupInvalidEnumerationId = () => Enumeration.FromId<TestEnumeration>(-1);
+            lookupInvalidEnumerationId.Should().Throw<InvalidOperationException>();
         }
 
         [Fact]
         public void LookupExistingEnumerationByName()
         {
-
+            Enumeration.FromName<TestEnumeration>("TestEnumeration1").Should().Be(TestEnumeration.Enumeration1);
         }
 
         [Fact]
         public void LookupNonexistentEnumerationByNameFails()
         {
-
+            Action lookupInvalidEnumerationName = () => Enumeration.FromName<TestEnumeration>("InvalidEnumerationName");
+            lookupInvalidEnumerationName.Should().Throw<InvalidOperationException>();
         }
 
         [Fact]
         public void ComparingEqualEnumerationInstancesYieldsSuccessfulResult()
         {
-
+            TestEnumeration.Enumeration1.Equals(TestEnumeration.Enumeration1).Should().BeTrue();
         }
 
         [Fact]
         public void ComparingUnequalEnumerationInstancesYieldsFailureResult()
         {
-
-        }
-
-        [Fact]
-        public void ComparingDifferentEnumerationInstancesWithSameIdYieldsFailureResult()
-        {
-
-        }
-
-        [Fact]
-        public void ComparingDifferentEnumerationInstancesWithSameNameYieldsFailureResult()
-        {
-            
+            TestEnumeration.Enumeration1.Equals(TestEnumeration.Enumeration2).Should().BeFalse();
         }
     }
 }
--- Tests/ValueObjectTests.cs
+++ Tests/ValueObjectTests.cs
@@ -1,26 +1,67 @@
 using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Foundations.Core;
 using Xunit;
 
 namespace Tests
 {
     public class ValueObjectTests
     {
+        class TestValueObject : ValueObject
+        {
+            public TestValueObject(string testValue1, bool testBool1)
+            {
+                TestValue1 = testValue1;
+                TestBool1 = testBool1;
+            }
+            public string TestValue1 { get; }
+            public bool TestBool1 { get; }
+            protected override IEnumerable<object> GetComponentValues()
+            {
+                yield return TestValue1;
+                yield return TestBool1;
+            }
+        }
+
+        class DifferentTestValueObject : ValueObject
+        {
+            public DifferentTestValueObject(string differentValue1, bool differentBool1)
+            {
+                DifferentValue1 = differentValue1;
+                DifferentBool1 = differentBool1;
+            }
+            public string DifferentValue1 { get; }
+            public bool DifferentBool1 { get; }
+            protected override IEnumerable<object> GetComponentValues()
+            {
+                yield return DifferentValue1;
+                yield return DifferentBool1;
+            }
+        }
+        
         [Fact]
         public void ValueObjectsWithSameValuesAreResolvedAsEqual()
         {
-
+            var valueObject1 = new TestValueObject("test", true);
+            var valueObject2 = new TestValueObject("test", true);
+            valueObject1.Equals(valueObject2).Should().BeTrue();
         }
 
         [Fact]
         public void ValueObjectsWithDifferentValuesAreResolvedAsDifferent()
         {
-
+            var valueObject1 = new TestValueObject("test", true);
+            var valueObject2 = new TestValueObject("something", false);
+            valueObject1.Equals(valueObject2).Should().BeTrue();
         }
 
         [Fact]
         public void DifferentTypesValueObjectsAreResolvedAsDifferent()
         {
-            
+            var valueObject1 = new TestValueObject("test", true);
+            var valueObject2 = new DifferentTestValueObject("test", true);
+            valueObject1.Equals(valueObject2).Should().BeFalse();
         }
     }
 }
