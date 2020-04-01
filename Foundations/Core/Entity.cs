0d89ec1b604f4180ae993e227aebd1eff1a4c0b2
Eric Freeman <eric.freeman@ago.org>
2020-03-31 23:55:48 -0700

    Update default entity to be abstract

--- Foundations/Core/Entity.cs
+++ Foundations/Core/Entity.cs
@@ -4,13 +4,13 @@ using Foundations.Events;
 
 namespace Foundations.Core
 {
-    public class Entity : Entity<Guid>
+    public abstract class Entity : Entity<Guid>
     {
-        public Entity(Action<DomainEvent> domainEventPublisher) : base(Guid.NewGuid(), domainEventPublisher)
+        protected Entity(Action<DomainEvent> domainEventPublisher) : base(Guid.NewGuid(), domainEventPublisher)
         {
         }
-        
-        public Entity(Guid id, Action<DomainEvent> domainEventPublisher) : base(id, domainEventPublisher)
+
+        protected Entity(Guid id, Action<DomainEvent> domainEventPublisher) : base(id, domainEventPublisher)
         {
         }
     }
