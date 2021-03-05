3920043073fe184fc56e6a7c6e3d61ad2282c2b4
Eric <SmeeK153@users.noreply.github.com>
2021-03-04 23:21:54 -0800

    Add docs for MediatR constructs

--- Domain.Design.Foundations.Extensions.DependencyInjection.MediatR/Extensions/MediatRDomainExtensions.cs
+++ Domain.Design.Foundations.Extensions.DependencyInjection.MediatR/Extensions/MediatRDomainExtensions.cs
@@ -1,19 +1,23 @@
 using System.Reflection;
+using Domain.Design.Foundations.Events;
+using Domain.Design.Foundations.Middleware;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Domain.Design.Foundations.Extensions
 {
     /// <summary>
-    /// 
+    /// Registers the specified <see cref="MediatRDomainEventManager"/> with the request pipeline and configures the
+    /// <see cref="DomainMiddleware"/> to manage <see cref="DomainEvent"/>s during the request lifecycle.
     /// </summary>
     public static partial class MediatRDomainExtensions
     {
         /// <summary>
-        /// 
+        /// Registers the specific <see cref="MediatRDomainEventManager"/> as the event manager for all requests.
         /// </summary>
-        /// <param name="services"></param>
-        /// <param name="assemblies"></param>
+        /// <param name="services">Collection of service descriptors</param>
+        /// <param name="assemblies"><see cref="Assembly"/> references to send to MediatR for registering any pertinent
+        /// implementations for defining events or handling events</param>
         /// <returns></returns>
         public static IServiceCollection AddMediatRDomainEvents(this IServiceCollection services, params Assembly[] assemblies)
         {
--- Domain.Design.Foundations.Extensions.DependencyInjection.MediatR/MediatRDomainEventManager.cs
+++ Domain.Design.Foundations.Extensions.DependencyInjection.MediatR/MediatRDomainEventManager.cs
@@ -1,36 +1,44 @@
+using System;
 using System.Threading.Tasks;
+using Domain.Design.Foundations.Core.Abstract;
 using Domain.Design.Foundations.Events;
 using MediatR;
 
 namespace Domain.Design.Foundations
 {
     /// <summary>
-    /// 
+    /// Forwards received <see cref="DomainEvent"/>s to the <see cref="INotificationHandler{TNotification}"/> during the
+    /// request lifecycle.
     /// </summary>
     public class MediatRDomainEventManager : DomainEventObserverManager
     {
         /// <summary>
-        /// 
+        /// Creates a new event manager that routes any <see cref="DomainEvent"/>s received by known
+        /// <see cref="IObserver{T}"/>s to the <see cref="INotificationHandler{TNotification}"/>.
         /// </summary>
-        /// <param name="mediator"></param>
-        public MediatRDomainEventManager(IMediator mediator)
+        /// <param name="mediator">MediatR instance to use for publishing <see cref="DomainEvent"/>s</param>
+        /// <param name="handler"><see cref="INotificationHandler{TNotification}"/> to received published
+        /// <see cref="DomainEvent"/>s</param>
+        public MediatRDomainEventManager(IMediator mediator, INotificationHandler<MediatRDomainEvent> handler)
         {
-            _mediator = mediator;
+            Mediator = mediator;
         }
 
         /// <summary>
-        /// 
+        /// Processing strategy for forwarding <see cref="DomainEvent"/>s to the
+        /// <see cref="INotificationHandler{TNotification}"/> for separate handling.
         /// </summary>
-        /// <param name="domainEvent"></param>
-        /// <returns></returns>
+        /// <param name="domainEvent"><see cref="DomainEvent"/> received from the <see cref="Entity{T}"/> the
+        /// <see cref="IObserver{T}"/> has been listening to</param>
         protected override async Task ExecuteEvent(DomainEvent domainEvent)
         {
-            await _mediator.Publish(domainEvent);
+            await Mediator.Publish(domainEvent);
         }
         
         /// <summary>
-        /// 
+        /// MediatR instance for publishing <see cref="DomainEvent"/>s to the
+        /// <see cref="INotificationHandler{TNotification}"/>.
         /// </summary>
-        private IMediator _mediator { get; }
+        private IMediator Mediator { get; }
     }
 }
